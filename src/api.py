dbb90764678621c7ea8cb76cd94c8aa984be5444
divingmastergit <sunshine@rsnweb.ch>
2022-09-13 14:44:57 +0000

    Initial commit

--- src/api.py
+++ src/api.py
@@ -1,9 +1,15 @@
+from queue import Empty
+import random
+import os
+from tkinter import image_names
 from src.ims import InMemoryStorage
 from flask import *
 from datetime import timedelta
 import sqlite3
 import hashlib
 from werkzeug.utils import secure_filename
+import magic
+
 
 
 app = Flask(__name__)
@@ -21,6 +27,7 @@ def Hash(String):
 @app.route('/home')
 @app.route('/')
 def home():
+    session.clear()
     return render_template('home.html', title='Home', app_version=version)
 
 
@@ -30,21 +37,31 @@ def upload_images():
 
 @app.route('/guess', methods=['GET', 'POST'])
 def guess():
-    list = data.get_all_words()
+    list = data.get_all_words_database()
     length = len(list)
-    for i in range(length):
-        list[i] = list[i].replace("\n", "")
-    if len(list) == 0:
-        flash("No Words uploaded yet.")
-    session['word'] 
+    if(length == 0):
+        flash("No Images are stored")
+        return redirect("/word")
+    if "word" not in session  and "number" not in session:
+        print("Test")
+        return render_template('guess.html', title='Guess', app_version=version)
+    
+    else:
+        print("Nothing in session")
+
+    #for i in range(length):
+        #list[i] = list[i].replace("\n", "")
+
     if request.method == "POST":
-        guesss_word = request.form['secretWord']  # note the "name" attribute of the <input> we have in HTML
-        print(guesss_word)
-        print(list)
-        if guesss_word in list:
+        guesss_word = request.form['secretWord']
+        right_word = session['word']
+        guesss_word =  "('" + guesss_word + "',)"
+        print(guesss_word + "Test")
+        if guesss_word == right_word:
             flash("You guessed right.")
-            print("right") 
-            #return redirect('/')  # redirect back to the main page
+            return redirect('/')
+        else:
+            flash("You guessed wrong")
     return render_template('guess.html', title='Guess', app_version=version)
 
 @app.route('/word', methods=['GET', 'POST'])
@@ -52,14 +69,59 @@ def word():
     
     if request.method == "POST":
         secret_word = request.form['secretWord']  # note the "name" attribute of the <input> we have in HTML
-        data.add_word(secret_word)
+        secret_word = request.form['secretWord']
+        image_file = request.files["image"]  # 'image' is the name of the input in HTML
+        image_bytes = image_file.stream.read()  # actual image content
+        image_content_type = image_file.content_type
+
+        data.add_image(image_bytes, image_content_type)
+        print("Imagae added added")
+        data.add_word_database(secret_word)
+        print("Word added")
+
         flash("Uploaded word " + repr(secret_word))
-        list = data.get_all_words()
-        length = len(list)
-        for i in range(length):
-            flash(list[i])
+
+        list = data.get_all_words_database()
+
+        return render_template('word.html', title='Guess', app_version=version, uploaded_image = image_file.filename)
         return redirect('/')  # redirect back to the main page
+        
     return render_template('word.html', title='Guess', app_version=version)
+    
+@app.route('/image', methods=['GET'])
+def get_image():
+    list = data.get_all_words_database()
+    lenght = len(list)
+    lenght = str(lenght)
+    f = open("images/" + lenght + ".png", "rb")
+    file_bytes = f.read()
+    mime = magic.Magic(mime=True)
+    return Response(file_bytes, mimetype=mime.from_file("images/" + lenght + ".png"))
 
+@app.route('/imageguess', methods=['GET'])
+def get_image_guess():
+    list = data.get_all_words_database()
+    if "word" not in session  and "number" not in session:
+        print("Nothing in session")
+        rand = random.randint(1, len(list))
+        randomword = str(list[rand-1])
+        randomword = randomword.replace("\n", "")
+        session['word'] = randomword
+        session['number'] =  rand
+        print(session['word'])
 
-
+    
+    else:
+        lenght = session['number']
+        lenght  = str(lenght)
+        f = open("images/" + lenght + ".png", "rb")
+        file_bytes = f.read()
+        mime = magic.Magic(mime=True)
+        print("Test")
+        return Response(file_bytes, mimetype=mime.from_file("images/" + lenght + ".png"))
+    lenght = session['number']
+    lenght  = str(lenght)
+    f = open("images/" + lenght + ".png", "rb")
+    file_bytes = f.read()
+    mime = magic.Magic(mime=True)
+    return Response(file_bytes, mimetype=mime.from_file("images/" + lenght + ".png"))
--- src/ims.py
+++ src/ims.py
@@ -1,24 +1,65 @@
-from pathlib import Path
+from importlib.resources import path
+import os
+import sqlite3
+from turtle import st
 class InMemoryStorage:
-    
+    #def __init__(self):
+        #spot = Path("words.txt")
+        #if spot.is_file():
+          #  print("File existes")
+       # else:
+       #     f = open("words.txt", "a")
+      #      f.close()
+
+    #def add_word(self, secret_word: str) -> None:
+    #    """ Store a secret word."""
+   #     f = open("words.txt", "a")
+   #     f.write(secret_word + "\n")
+   #     f.close()
+
+
+
+   # def get_all_words(self):
+  #      f = open("words.txt", "r")
+   #     storage = f.readlines()
+   #     f.close()
+   #     return storage
+
     def __init__(self):
-        spot = Path("words.txt")
-        if spot.is_file():
-            print("File existes")
+        if os.path.exists("words.db"):
+            print("Database already exists")
         else:
-            f = open("words.txt", "a")
-            f.close()
+            con01 = sqlite3.connect("words.db")
+            cursor = con01.cursor()
+            cursor.execute("CREATE TABLE WORDS (Word text);")
+            con01.close()
+            print("Database has been created sucessful")
+        if(os.path.isdir("images")):
+            print("Directory already exists.")
+        else:
+            os.path.os.mkdir("images")
 
-    def add_word(self, secret_word: str) -> None:
-        """ Store a secret word."""
-        f = open("words.txt", "a")
-        f.write(secret_word + "\n")
-        f.close()
 
-
-
-    def get_all_words(self):
-        f = open("words.txt", "r")
-        storage = f.readlines()
-        f.close()
-        return storage
+    def add_word_database(self, secret_word: str) -> None:
+            con = sqlite3.connect("words.db")
+            cur = con.cursor()
+            cur.execute("INSERT INTO WORDS (Word) VALUES(?);", (secret_word,))
+            con.commit()
+            con.close()
+            print("Word has been added")
+    
+    
+    def get_all_words_database(self) -> list[str]:
+        con = sqlite3.connect("words.db")
+        cur = con.cursor()
+        cur.execute("SELECT Word FROM Words")
+        words = cur.fetchall()
+        return words
+    
+    def add_image(self, file: bytes, file_type: str):
+        file_type = file_type.replace("image/", "")
+        file_name = len(os.listdir("images")) + 1
+        file_name = str(file_name)
+        f = open("images/" + file_name + ".png" , "wb")
+        f.write(file)
+            
